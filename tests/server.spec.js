1db5377f9559d9efba90ca38466f68edead56b14
DICAVI86 <delcastillovial@gmail.com>
2024-11-09 12:56:51 -0300

    Se realizo la tercera prueba y paso. Se ajustaron algunas cosas del codigo ya que tiró error muchas veces en la parte de agregar el café.

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -46,6 +46,39 @@ describe('GET /cafes/:id', () => {
 });
 
 //03
+describe('POST /cafes', () => {
+    it('debe agregar un café correctamente y devolver un status 201', async () => {
+      const newCafe = {
+        id: 5,   // Cambié el id para evitar conflicto con los existentes
+        nombre: "Latte"
+      };
+  
+      const response = await request(app).post('/cafes').send(newCafe);
+  
+      // Verificar que el código de estado sea 201
+      expect(response.status).toBe(201);
+  
+      // Verificar que el café agregado esté en la respuesta
+      expect(response.body).toEqual(expect.arrayContaining([newCafe]));
+    });
+  
+    it('debe devolver un status 400 si ya existe un café con el mismo id', async () => {
+      const newCafe = {
+        id: 1,  // Usamos un id que ya existe
+        nombre: "Capuccino"
+      };
+  
+      const response = await request(app).post('/cafes').send(newCafe);
+  
+      // Verificar que el código de estado sea 400
+      expect(response.status).toBe(400);
+  
+      // Verificar que el mensaje de error es el esperado
+      expect(response.body.message).toBe("Ya existe un cafe con ese id");
+    });
+  });
+  
+  
 
 
 

4a66b779cff8a5fbba772c05df3468c09a09d426
DICAVI86 <delcastillovial@gmail.com>
2024-11-09 12:06:53 -0300

    Se realizo la primera prueba y despues de varios ajustes que lanzaban un error, se arreglo y pasó

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,24 @@
-const request = require("supertest");
-const server = require("../index");
+const request = require('supertest'); 
+const app = require('../index');  
 
-describe("Operaciones CRUD de cafes", () => {
 
-});
+//01
+describe('GET /cafes', () => {
+    it('debe devolver un status 200 y un array de cafés', async () => {
+      const response = await request(app).get('/cafes');
+  
+      expect(response.status).toBe(200);
+
+      expect(Array.isArray(response.body)).toBe(true);
+
+      if (response.body.length > 0) {
+        const cafe = response.body[0];
+        expect(cafe).toHaveProperty('id');
+        expect(cafe).toHaveProperty('nombre');
+
+      }
+    });
+  });
+
+
+  
